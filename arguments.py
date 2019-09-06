10c1530c633e84aab38a713dd72be0194a7f854d
AyyItsAljo <aljaz.medic@gmail.com>
2019-09-06 22:45:27 +0200

    Update gitignore to ignore private oauth data and pycharm .idea

--- arguments.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-def arg_init():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-prune_temp','-p', help='prune temp folder', action='store_true')
-	parser.add_argument('--calendar_name', help="Calendar name (defaults to School2)", dest="cal_name", type=str, default="School2")
-	parser.add_argument('-remove_calendar', help="Removes the calendar befor running.", dest="rm_cal", required=False, action='store_true')
-	return parser.parse_args()
--- notifier.py
+++
@@ -1,2 +0,0 @@
-def send_notification(txt):
-	return
