60d84be61d1f55caebbdc25f7ca2c1140756b2d7
AyyItsAljo <aljaz.medic@gmail.com>
2019-09-09 23:25:03 +0200

    Fixed triple adding events

--- event_handler.py
+++ event_handler.py
@@ -12,15 +12,15 @@ def update_date(google_cal_service: GoogleCalendarService, ea_service: EAssistan
 	if len(dates_to_update) == 1:
 		dates_to_update.append(dates_to_update[0])
 	eas_events = ea_service.get_school_events(dates_to_update[0], dates_to_update[1]+datetime.timedelta(days=1))
-	for date_to_update in dates_to_update:
-		time_min, time_max = gstrptime(eas_events["time_boundary"]["min"]), gstrptime(eas_events["time_boundary"]["max"])
-		print(time_min, time_max)
-		events = eas_events.get("events", [])
-		# TODO add time boundary, add notify on special, make sure to prune all events before adding new ones, predict meal time
-		for i, e in enumerate(events):
-			logging.info(f"{date_to_update}, event {i}.")
+	#for date_to_update in dates_to_update:
+	time_min, time_max = gstrptime(eas_events["time_boundary"]["min"]), gstrptime(eas_events["time_boundary"]["max"])
+	print(time_min, time_max)
+	events = eas_events.get("events", [])
+	# TODO add time boundary, add notify on special, make sure to prune all events before adding new ones, predict meal time
+	for i, e in enumerate(events):
+		logging.info(f"Event {i}.")
 
-			body = ef.google_event_body_from_parsed_event(e)
-			google_cal_service.add_event(body)
-			sleep(1)
+		body = ef.google_event_body_from_parsed_event(e)
+		google_cal_service.add_event(body)
+		sleep(1)
 
