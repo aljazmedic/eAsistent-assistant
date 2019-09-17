bc3aac301f16a7200b22cb7a41736483750c2bfb
AyyItsAljo <aljaz.medic@gmail.com>
2019-09-17 18:56:16 +0200

    Added run with different arguments.

--- arguments.py
+++ arguments.py
@@ -15,6 +15,6 @@ def run_args_init():
 	parser.add_argument('--cname', help="Calendar name (defaults to School2)", dest="cal_name", type=str, default="School2")
 	parser.add_argument('--rm_cal', '-rc', help="Removes the calendar befor running.", dest="rm_cal", required=False, action='store_false')
 	parser.add_argument('--file', '-f', help="Log file name.", dest="log_file_name", required=False, type=str, default='logs/%s.log')
-	parser.add_argument('--log_type', help="Log Mode. Either append or write.", dest="log_mode", required=False, choices=['w', 'a'], type=str, default='w')
+	parser.add_argument('--log_type', help="Log Mode. Either append or write.", dest="log_mode", name='log file name',required=False, choices=['w', 'a'], type=str, default='w')
 
 	return parser.parse_args()
