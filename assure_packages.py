9e0c2674610ab1587c6ac5f18bb5ffa2bb429f29
AyyItsAljo <aljaz.medic@gmail.com>
2019-12-22 13:39:10 +0100

    Re-did it with subprocess module

--- assure_packages.py
+++ assure_packages.py
@@ -1,16 +1,22 @@
-import pip._internal
+from pip import _internal
 import pip
+
+import subprocess
+import sys, os
 
 
 def install_requirements():
-    if hasattr(pip, 'main'):
-        pip.main(["install", "--user", "--upgrade", "pip==9.0.3"])
-        pip.main(['install', '-r', 'requirements.txt'])
-    else:
-        pip._internal.main(["install", "--user", "--upgrade", "pip==9.0.3"])
-        pip._internal.main(['install', '-r', 'requirements.txt'])
+	subprocess.check_call([sys.executable, "-m", "pip", "install", "-q", '-r', 'requirements.txt'])
+
+
+def install_pip(version=None):
+	if version is None:
+		subprocess.check_call([sys.executable, "-m", "pip", "install", "-q", "--user", "--upgrade", "pip"])
+	else:
+		subprocess.check_call([sys.executable, "-m", "pip", "install", "-q", "--user", "--upgrade", f"pip=={version}"])
 
 
 # Example
 if __name__ == '__main__':
-    install_requirements()
+	install_pip()
+	install_requirements()
